014cf2d35e3da48e9416d335df883bcd1b8911e0
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-03-20 08:54:36 -0600

    Function that return an array of integers that are sorted in ascending order and the squares of the original integers.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function sortedSquaredArray(array) {
+  square = []
   // Write your code here.
-  return [];
+  for(pos of array) {
+    p = Math.sign(pos)
+    if(p === -1){
+      return console.log([])
+    }
+  }
+
+  array.sort((a, b) => a - b)
+  for(arr of array) {
+    square.push(Math.pow(arr, 2)) 
+  }
+  return console.log(square);
 }
 
+sortedSquaredArray([2,8,5,7,1,3])
 // Do not edit the line below.
 exports.sortedSquaredArray = sortedSquaredArray;

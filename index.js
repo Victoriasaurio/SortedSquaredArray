8d7fbf53ce642126075daca3ae5f267dc8acabd2
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-03-20 11:05:32 -0600

    Corrections were made to the function

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 function sortedSquaredArray(array) {
+  absoluto = []
   square = []
   // Write your code here.
-  for(pos of array) {
-    p = Math.sign(pos)
-    if(p === -1){
-      return console.log([])
-    }
-  }
-
-  array.sort((a, b) => a - b)
-  for(arr of array) {
+  array.map((num) => absoluto.push(Math.abs(num)))
+  absoluto.sort((a, b) => a - b)
+  for(arr of absoluto) {
     square.push(Math.pow(arr, 2)) 
   }
   return console.log(square);
 }
 
-sortedSquaredArray([2,8,5,7,1,3])
+sortedSquaredArray([2,8,5,-7,1,3,9,6,-4])
 // Do not edit the line below.
 exports.sortedSquaredArray = sortedSquaredArray;
